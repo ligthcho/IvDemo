3bccda3ee0796fcdf8ed03747f4722780894d4a7
Sincho <853358540@qq.com>
2019-03-29 17:33:26 +0800

    44

--- ivDemo/Models/SupplierType.cs
+++ ivDemo/Models/SupplierType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace ivDemo.Models
 	/// </summary>
     public class SupplierType
     {
+		[Key]
 		/// <summary>
 		/// 类型编号
 		/// </summary>
